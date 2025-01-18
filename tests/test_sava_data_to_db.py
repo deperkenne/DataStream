5b05b68a15f7326172d2fc1be8e4719397f577a0
deperkenne <tchofodep@gmail.com>
2025-01-18 22:13:50 +0100

    change src repo to vitingApp

--- tests/test_sava_data_to_db.py
+++ tests/test_sava_data_to_db.py
@@ -1,24 +1,28 @@
 import pytest
-import re
+from sqlalchemy.testing.plugin.plugin_base import assertions
+from sparkstream.votingApp.insertdatatotables import *
+
 
 
 
 def test_return_correctly_list_of_candidates():
     # given
     BASE_URL = 'https://randomuser.me/api/?nat=gb'
+    list_gender_expected = setUpListCandidates()
+    i = 0
 
 
-
-    # when SUT
-    list_candidate = ()
-
-
-
-
-
-    # then
+    while i < 3:
+        # when SUT
+        list_candidate = get_data(BASE_URL)
+        list_genders = list(map(lambda candidate: candidate["gender"],list_candidate))
+        # then
+        assert set(list_gender_expected) == set(list_genders),"error not same containt"
+        i = i +1
 
 
+def setUpListCandidates():
+    return ["male","male","female"]
 
 
 
